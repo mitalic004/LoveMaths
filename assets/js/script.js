93fcc3ee996f090ee57fd333bce58ab066126c22
mc5g20 <131391810+mc5g20@users.noreply.github.com>
2024-12-10 20:05:21 +0000

    Update division display to ensure answer is integer

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,6 +140,7 @@ function displayMultiplyQuestion(operand1, operand2) {
  * Displays division question to user.
  */
 function displayDivideQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "/";
